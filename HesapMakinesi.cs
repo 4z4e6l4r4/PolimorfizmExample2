06df314e3375e35136fdfc05a58fce8832ab2a58
Azra <yukselazrael@gmail.com>
2024-01-29 23:22:17 +0300

    Added Project

---
+++ HesapMakinesi.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorphizmExample2
+{
+    internal class HesapMakinesi
+    {
+        public int Topla(int sayi1, int sayi2) //Derleme Zamanlı Polimorfizm
+        {
+            return sayi1 + sayi2;
+        }
+        public double Topla(double sayi1, double sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using PolimorphizmExample2;
+
+namespace PolimorfizmExample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HesapMakinesi hesapMakinesi = new HesapMakinesi();
+            int sonuc1 = hesapMakinesi.Topla(2,7);
+            Console.WriteLine("Sonuc: " + sonuc1);
+
+            double sonuc2 = hesapMakinesi.Topla(3,20.4);
+            Console.WriteLine("Sonuc: " + sonuc2);
+
+        }
+    }
+}
